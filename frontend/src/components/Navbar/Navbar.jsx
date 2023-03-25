8ca9ecc00c53c79c94e4f963e690c4b565b3f9f0
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-03-25 17:06:12 -0400

    edited nav bar a little

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Navbar.scss'
+import {images} from '../../constants/'
 
 const Navbar = () => {
   return (
-    <div>
-    </div>
+    <nav  className='app__navbar'>
+      <div  className='app__navbar-logo'>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul  className='app__navbar-links'>
+        {['Home', 'About', 'Work', 'Skills', 'Contact'].map((item, index) => (
+          <li  className='app__flex p-text' key={index}>
+            <div>
+              <a href={`#${item.toLowerCase()}`}>{item}</a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
