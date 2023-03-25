bc82b4312aa6ee90cfaabfd5988d663220ffeee1
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-03-25 02:30:48 -0400

    initial commit

---
+++ backend_sanity/dg_portfolio/schemas/index.ts
@@ -0,0 +1,11 @@
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import experience from './experience'
+import skills from './skills'
+import testimonials from './testimonials'
+import workExperience from './workExperience'
+import works from './works'
+
+
+export const schemaTypes = [about, brands, contact, experience, skills, testimonials, workExperience, works]
