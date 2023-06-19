36fc3f598c1d0fb5b7bdd4185a9072f8eb2ad33c
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-06-19 14:15:34 -0400

    corrected bugs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-
-import App from './App';
 import './index.css';
+import App from './App';
+import { createRoot } from 'react-dom';
 
 const root = document.getElementById('root');
 createRoot(root).render(
