86bf0e9a642673ec64b157cf85d69b9357886a00
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-06-26 23:22:53 -0400

    updated tab

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { BsTwitter, BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
 import './SocialMedia.scss'
 
 function SocialMedia() {
   return (
+
     <div className='app__social'>
-      <div>
+      <div className='icon'>
         <a href="https://twitter.com/DGuardiola17" target="_blank">
           <BsTwitter />
         </a>
       </div>
-      <div>
+      <div className='icon'> 
         <a href="https://www.linkedin.com/in/diego-guardiola/" target="_blank">
           <BsLinkedin />
         </a>
       </div>
-      <div>
+      <div className='icon'>
         <a href="https://github.com/diegoguardiola" target="_blank">
           <BsGithub />
         </a>
       </div>
-      <div>
+      <div className='icon'>
       <a href="https://www.instagram.com/dguardiola/" target="_blank">
         <BsInstagram />
       </a>
